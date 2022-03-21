4c4b62e09ae994a7f70dc37bc67c1858718799c3
Sean Cleveland <sean.b.cleveland@gmail.com>
2022-03-21 12:02:11 -0600

    Use 0,0 grid cell for mask value detection - works for temp and rainfall plots.

--- plot_geotiff.py
+++ plot_geotiff.py
@@ -18,9 +18,10 @@ import matplotlib.ticker as mtick
 
 my_image = georaster.SingleBandRaster(sys.argv[1])
 fig, ax = plt.subplots()
-masked = np.ma.masked_equal(my_image.r, 0.)
+#masked = np.ma.masked_equal(my_image.r, 0.)
+masked = np.ma.masked_equal(my_image.r, my_image.r[0][0])
 ax.xaxis.set_major_formatter(mtick.FormatStrFormatter('%.1f'))
-shw = ax.imshow(masked,extent=my_image.extent,cmap=sys.argv[3],vmin=-15,vmax=40)
+shw = ax.imshow(masked,extent=my_image.extent,cmap=sys.argv[3],vmin=sys.argv[5],vmax=sys.argv[6])
 plt.colorbar(shw,extend = 'both',format='%.1f',shrink=0.8).minorticks_on()
 name=sys.argv[1].split('.')[0]
 title_str =  sys.argv[4]#' '.join(elem.capitalize() for elem in name.split('_'))
